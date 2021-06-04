fb258b48fd30f6cdeb2a4625003aa25353b9b4f2
TimoThans33 <timotopper@gmail.com>
2021-06-04 12:51:22 +0200

    http server implemented

--- code/lib/api/include/api.h
+++ code/lib/api/include/api.h
@@ -2,12 +2,16 @@
 #define API_H
 
 #include <boost/asio.hpp>
+//#include <boost/filesystem.hpp>
+
 #include <iostream>
 #include <string>
 #include <memory>
 #include <thread>
 #include <atomic>
 #include <mutex>
+
+#include <fstream>
 
 const unsigned int DEFAULT_THREAD_POOL_SIZE = 2;
 
@@ -174,6 +178,10 @@ namespace machyAPI
         /*
          * connect to a server in asynchronous manner
          */
+        int asynchronous_httpserver();
+        /*
+         * http server
+         */
         class aSyncTCPClient : public boost::noncopyable
         {
             /*
@@ -207,6 +215,72 @@ namespace machyAPI
                 std::unique_ptr<boost::asio::io_service::work> m_work;
                 std::unique_ptr<std::thread> m_thread;
         };
+
+        class http_service
+        {
+            /*
+             * service class that provides the implementation of the 
+             * http protocol.
+             */
+            static const std::map<unsigned int, std::string>http_status_stable;
+            public:
+                http_service(std::shared_ptr<boost::asio::ip::tcp::socket> sock) :
+                    m_sock(sock), m_request(4096), m_response_status_code(200), m_resource_size_bytes(0)
+                {};
+                void StartHandling();
+                /*
+                 * initiate asynchronous communication session with the
+                 * client connected to the socket
+                 */
+            private:
+                void on_request_line_recieved(const boost::system::error_code& ec,
+                    std::size_t bytes_transferred);
+                /*
+                 * process the http request line 
+                 * supported: GET
+                 */
+                void on_headers_received(const boost::system::error_code& ec,
+                    std::size_t bytes_transferred);
+                /*
+                 * process and store the request header block
+                 */
+                void process_request();
+                /*
+                 * read the contents of the requested resource from
+                 * the file system and store it in a buffer
+                 */
+                void send_response();
+                /*
+                 * response message and send it to the client
+                 */
+                void on_response_sent(const boost::system::error_code& ec,std::size_t bytes_transferred);
+                /*
+                 * after response sent kill the socket and cleanup
+                 */
+                void on_finish(){ delete this; }
+                std::shared_ptr<boost::asio::ip::tcp::socket> m_sock;
+                boost::asio::streambuf m_request;
+                std::map<std::string, std::string> m_request_headers;
+                std::string m_requested_resource;
+
+                std::unique_ptr<char[]> m_resource_buffer;
+                unsigned int m_response_status_code;
+                std::size_t m_resource_size_bytes;
+                std::string m_response_headers;
+                std::string m_response_status_line;
+        };
+
+        const std::map<unsigned int, std::string>
+        http_status_table = 
+        {
+            { 200, "200 OK" },
+            { 404, "404 Not Found" },
+            { 413, "413 Request Entity Too Large" },
+            { 500, "500 Server Error" },
+            { 501, "501 Not Implemented" },
+            { 505, "505 HTTP Version Not Supported" }
+        };
+
         class service
         {
             /*
@@ -274,6 +348,15 @@ namespace machyAPI
                 /*
                 * start accepting incoming connection requests
                 */
+                void start_http(){
+                    std::cout<<"start http acceptor\n";
+                    m_acceptor.listen();
+                    InitAccept_http();
+                }
+                /*
+                 * start accepting incoming http connection
+                 * requests
+                 */
                 void stop(){ m_isStopped.store(true);}
                 /*
                 * stop accepting incoming connection requests
@@ -284,12 +367,23 @@ namespace machyAPI
                 * constructs an active socket object and initiates
                 * the asynchronous accept operation.
                 */
+                void InitAccept_http();
+                /*
+                 * constructs an active socket object and 
+                 * initiates the asynchronous
+                 */
                 void onAccept(const boost::system::error_code& ec,
                     std::shared_ptr<asio::ip::tcp::socket> sock);
                 /*
                 * if asynchronous operation was succesfull an instance
                 * of the service class is created.
                 */
+                void onAccept_http(const boost::system::error_code& ec,
+                    std::shared_ptr<asio::ip::tcp::socket> sock);
+                /*
+                 * if asynchronous operation was succesfull an instance
+                 * of the http_service class is created
+                 */
                 asio::io_service&m_ios;
                 asio::ip::tcp::acceptor m_acceptor;
                 std::atomic<bool>m_isStopped;
@@ -312,10 +406,14 @@ namespace machyAPI
                 * starts a server on specified port and listens for asynchronous
                 * operations on defined number of threads.
                 */
+                void start_http(unsigned short port_num, unsigned int thread_pool_size);
+                /*
+                 * starts asynchronous http server on specified port
+                 */
                 void stop();
                 /*
-                * stops the acceptor instance and stops all trheads
-                * defined in start.
+                 * stops the acceptor instance and stops all trheads
+                 * defined in start.
                 */
             private:
                 asio::io_service m_ios;
